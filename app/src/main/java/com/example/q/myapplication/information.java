cbb9f7859371bc1c73566bae30c246f4bc9f8ffc
배성범 <bsb0579@naver.com>
2018-07-02 20:00:49 +0900

    hihi

--- app/src/main/java/com/example/q/myapplication/information.java
+++ app/src/main/java/com/example/q/myapplication/information.java
@@ -1,14 +1,25 @@
 package com.example.q.myapplication;
 
+import android.content.ContentProviderOperation;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class information extends AppCompatActivity {
 
@@ -17,18 +28,176 @@ public class information extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
 
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("Name");
-        String phone = intent.getStringExtra("Phone");
-        final int index = intent.getIntExtra("Index", 0);
 
-        TextView nameView = (TextView) findViewById(R.id.detai_name);
-        TextView phoneView = (TextView) findViewById(R.id.detai_phone);
+        Intent intent = getIntent();
+        final String name = intent.getStringExtra("Name");
+        final String phone = intent.getStringExtra("Phone");
+
+        TextView nameView = (TextView) findViewById(R.id.info_name);
+        TextView phoneView = (TextView) findViewById(R.id.info_phone);
 
         nameView.setText(name);
         phoneView.setText(phone);
 
         Button btnDelete = (Button) findViewById(R.id.delete_info);
+        Button btnoutofway = (Button) findViewById(R.id.out_info);
+        Button btnChange = (Button) findViewById(R.id.change_info);
+        btnoutofway.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent outIntent = new Intent(getApplicationContext(), phonebook.class);
+                setResult(RESULT_OK, outIntent);
+                finish();
+            }
+        });
+
+
+
+        btnChange.setOnClickListener( new View.OnClickListener(){
+            public void onClick(View v){
+                Context context = information.this;
+                LinearLayout layout = new LinearLayout(context);
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                AlertDialog.Builder add = new AlertDialog.Builder(information.this);
+
+                add.setTitle("수정하기");
+                //add.setMessage("이름");
+                final TextView name2 = new TextView(context);
+                name2.setText("이름");
+                layout.addView(name2);
+                final EditText name1 = new EditText(context);
+                name1.setHint(name);
+                name1.setText(name);
+                layout.addView(name1);
+                //add.setView(name1);
+                final TextView phone2 = new TextView(context);
+                phone2.setText("전화번호");
+                layout.addView(phone2);
+                final EditText phone1 = new EditText(context);
+                phone1.setInputType(0x00000003);
+                phone1.setHint(phone);
+                phone1.setText(phone);
+                layout.addView(phone1);
+                //add.setView(phone1);
+
+                add.setView(layout);
+
+                add.setPositiveButton("확인", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if ( name1.getText().toString().length() == 0 ) {
+                            AlertDialog.Builder add2 = new AlertDialog.Builder(information.this);
+                            add2.setTitle("공백입니다");
+                            add2.setNegativeButton("취소", new DialogInterface.OnClickListener(){
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+
+                            add2.show();
+
+
+                        } else {
+                            final String value1 = name1.getText().toString();
+                            final String value2 = phone1.getText().toString();
+                            Thread thread2 = new Thread(){
+                                @Override
+                                public void run() {
+
+                                    ArrayList<ContentProviderOperation> list = new ArrayList<>();
+                                    try{
+                                        list.add(
+                                                ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                                                        .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                                                        .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                                                        .build()
+                                        );
+
+                                        list.add(
+                                                ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                                                        .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, value1)   //이름
+
+                                                        .build()
+                                        );
+
+                                        list.add(
+                                                ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                                                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, value2)           //전화번호
+                                                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE  , ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)   //번호타입(Type_Mobile : 모바일)
+
+                                                        .build()
+                                        );
+
+                                        list.add(
+                                                ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                                                        .withValue(ContactsContract.CommonDataKinds.Email.DATA  , "")  //이메일
+                                                        .withValue(ContactsContract.CommonDataKinds.Email.TYPE  , ContactsContract.CommonDataKinds.Email.TYPE_WORK)     //이메일타입(Type_Work : 직장)
+
+                                                        .build()
+                                        );
+
+                                        getApplicationContext().getContentResolver().applyBatch(ContactsContract.AUTHORITY, list);  //주소록추가
+                                        list.clear();   //리스트 초기화
+                                    }catch(RemoteException e){
+                                        e.printStackTrace();
+                                    }catch(OperationApplicationException e){
+                                        e.printStackTrace();
+                                    }
+                                }
+                            };
+                            thread2.start();
+                            try{thread2.join();}catch (InterruptedException e){}
+
+                            Uri contactUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phone));
+                            Cursor cur = getContentResolver().query(contactUri, null, null, null, null);
+                            try {
+                                if (cur.moveToFirst()) {
+                                    do {
+                                        if (cur.getString(cur.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME)).equalsIgnoreCase(name)) {
+                                            String lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+                                            Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey);
+                                            getContentResolver().delete(uri, null, null);
+                                        }
+
+                                    } while (cur.moveToNext());
+                                }
+                            } catch (Exception e) {
+                                System.out.println(e.getStackTrace());
+                            } finally {
+                                cur.close();
+                            }
+
+                            Intent outIntent = new Intent(getApplicationContext(), phonebook.class);
+                            setResult(2, outIntent);
+                            finish();
+
+                        }
+                    }
+                });
+
+                add.setNegativeButton("취소", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                add.show();
+                }
+        });
+
+
+
         btnDelete.setOnClickListener( new View.OnClickListener(){
             public void onClick(View v){
                 AlertDialog.Builder check = new AlertDialog.Builder(information.this);
@@ -42,9 +211,27 @@ public class information extends AppCompatActivity {
                 check.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri contactUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phone));
+                        Cursor cur = getContentResolver().query(contactUri, null, null, null, null);
+                        try {
+                            if (cur.moveToFirst()) {
+                                do {
+                                    if (cur.getString(cur.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME)).equalsIgnoreCase(name)) {
+                                        String lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+                                        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey);
+                                        getContentResolver().delete(uri, null, null);
+                                    }
+
+                                } while (cur.moveToNext());
+                            }
+                        } catch (Exception e) {
+                            System.out.println(e.getStackTrace());
+                        } finally {
+                            cur.close();
+                        }
+
                         Intent outIntent = new Intent(getApplicationContext(), phonebook.class);
-                        outIntent.putExtra("index_del", index);
-                        setResult(RESULT_OK, outIntent);
+                        setResult(1, outIntent);
                         finish();
                     }
                 });
--- app/src/main/java/com/example/q/myapplication/phonebook.java
+++ app/src/main/java/com/example/q/myapplication/phonebook.java
@@ -1,14 +1,25 @@
 package com.example.q.myapplication;
+import android.content.ContentProviderOperation;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +31,7 @@ public class phonebook extends AppCompatActivity implements View.OnClickListener
     ArrayList<Map<String, String>> dataList;
     ListView mListview;
     private Button mBtnAddress;
+    private Button maddAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,56 +41,226 @@ public class phonebook extends AppCompatActivity implements View.OnClickListener
 
         mListview = (ListView) findViewById(R.id.listview);
         mBtnAddress = (Button) findViewById(R.id.btnAddress);
+        maddAddress = (Button) findViewById(R.id.button2);
         mBtnAddress.setOnClickListener(this);
+        maddAddress.setOnClickListener(this);
         mListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent_detail = new Intent(getApplicationContext(), information.class);
                 intent_detail.putExtra("Name", dataList.get(i).get("name"));
                 intent_detail.putExtra("Phone", dataList.get(i).get("phone"));
-                intent_detail.putExtra("Index", i);
                 startActivityForResult(intent_detail, 0);
             }
         });
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(0,1, data);
+        super.onActivityResult(0,2, data);
+        switch(resultCode){
+            case 1:
+                refresh();
+                break;
+            case 2:
+                refresh();
+                break;
+        }
+
+    }
+
+    public void deleteContact(Context ctx, String phone, String name) {
+        Uri contactUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phone));
+        Cursor cur = ctx.getContentResolver().query(contactUri, null, null, null, null);
+        try {
+            if (cur.moveToFirst()) {
+                do {
+                    if (cur.getString(cur.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME)).equalsIgnoreCase(name)) {
+                        String lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+                        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey);
+                        ctx.getContentResolver().delete(uri, null, null);
+                    }
+
+                } while (cur.moveToNext());
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        } finally {
+            cur.close();
+        }
+    }
+
+    public void refresh(){
+        dataList = new ArrayList<Map<String,String>>();
+        Cursor c = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null,
+                ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " asc");
+        while (c.moveToNext()) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            // 연락처 id 값
+            String id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+            // 연락처 대표 이름
+            String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
+            map.put("name", name);
+            // ID로 전화 정보 조회
+            Cursor phoneCursor = getContentResolver().query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id,
+                    null, null);
+            // 데이터가 있는 경우
+            String number = "";
+            if (phoneCursor.moveToFirst()) {
+                number = phoneCursor.getString(phoneCursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER));
+                map.put("phone", number);
+            }
+            phoneCursor.close();
+            dataList.add(map);
+        }// end while
+        c.close();
+        SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),
+                dataList,
+                android.R.layout.simple_list_item_2,
+                new String[]{"name", "phone"},
+                new int[]{android.R.id.text1, android.R.id.text2});
+        mListview.setAdapter(adapter);
+    }
+
+
+    //연락처 추가
+    public void ContactAdd(final String name11, final String phone11){
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+
+                ArrayList<ContentProviderOperation> list = new ArrayList<>();
+                try{
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                                    .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                                    .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                                    .build()
+                    );
+
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                    .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                                    .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name11)   //이름
+
+                                    .build()
+                    );
+
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                    .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phone11)           //전화번호
+                                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE  , ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)   //번호타입(Type_Mobile : 모바일)
+
+                                    .build()
+                    );
+
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                    .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                                    .withValue(ContactsContract.CommonDataKinds.Email.DATA  , "")  //이메일
+                                    .withValue(ContactsContract.CommonDataKinds.Email.TYPE  , ContactsContract.CommonDataKinds.Email.TYPE_WORK)     //이메일타입(Type_Work : 직장)
+
+                                    .build()
+                    );
+
+                    getApplicationContext().getContentResolver().applyBatch(ContactsContract.AUTHORITY, list);  //주소록추가
+                    list.clear();   //리스트 초기화
+                }catch(RemoteException e){
+                    e.printStackTrace();
+                }catch(OperationApplicationException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        try{thread.join();}catch (InterruptedException e){}
+    }
+
+
     public void onClick(View v){
         switch (v.getId()) {
             case R.id.btnAddress:
-                dataList = new ArrayList<Map<String,String>>();
-                Cursor c = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
-                        null, null, null,
-                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " asc");
-                while (c.moveToNext()) {
-                    HashMap<String, String> map = new HashMap<String, String>();
-                    // 연락처 id 값
-                    String id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
-                    // 연락처 대표 이름
-                    String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
-                    map.put("name", name);
-                    // ID로 전화 정보 조회
-                    Cursor phoneCursor = getContentResolver().query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id,
-                            null, null);
-                    // 데이터가 있는 경우
-                    String number = "";
-                    if (phoneCursor.moveToFirst()) {
-                        number = phoneCursor.getString(phoneCursor.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        map.put("phone", number);
+                refresh();
+                break;
+
+            case R.id.button2:
+                Context context = phonebook.this;
+                LinearLayout layout = new LinearLayout(context);
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                AlertDialog.Builder add = new AlertDialog.Builder(phonebook.this);
+
+                add.setTitle("추가하기");
+                //add.setMessage("이름");
+                final TextView name2 = new TextView(context);
+                name2.setText("이름");
+                layout.addView(name2);
+                final EditText name1 = new EditText(context);
+                //name1.setHint("이름");
+                layout.addView(name1);
+                //add.setView(name1);
+                final TextView phone2 = new TextView(context);
+                phone2.setText("전화번호");
+                layout.addView(phone2);
+                final EditText phone1 = new EditText(context);
+                phone1.setInputType(0x00000003);
+                //phone1.setHint("전화번호");
+                layout.addView(phone1);
+                //add.setView(phone1);
+
+                add.setView(layout);
+
+                add.setPositiveButton("확인", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if ( name1.getText().toString().length() == 0 ) {
+                            AlertDialog.Builder add2 = new AlertDialog.Builder(phonebook.this);
+                            add2.setTitle("공백입니다");
+                            add2.setNegativeButton("취소", new DialogInterface.OnClickListener(){
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+
+                            add2.show();
+
+
+                        } else {
+                            String value1 = name1.getText().toString();
+                            String value2 = phone1.getText().toString();
+                            ContactAdd(value1, value2);
+                            refresh();
+                        }
                     }
-                    phoneCursor.close();
-                    dataList.add(map);
-                }// end while
-                c.close();
-                SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),
-                        dataList,
-                        android.R.layout.simple_list_item_2,
-                        new String[]{"name", "phone"},
-                        new int[]{android.R.id.text1, android.R.id.text2});
-                mListview.setAdapter(adapter);
+                });
+
+                add.setNegativeButton("취소", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                add.show();
+
+                refresh();
+                break;
+
+
         }
     }
 
